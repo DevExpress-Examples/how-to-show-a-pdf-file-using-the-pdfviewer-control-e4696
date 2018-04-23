d9836de688cf287e2ce3ce0cd7e01f764527052e
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to show a PDF file using the PdfViewer control
+
+
+<p>The following example illustrates how to load an existing PDF file into the PdfViewer control at runtime.<br />To do this, call the <a href="https://documentation.devexpress.com/#windowsforms/DevExpressXtraPdfViewerPdfViewer_LoadDocumenttopic">PdfViewer.LoadDocument </a> method. </p>
+
+<br/>
+
+
---
+++ VB/PdfViewerSample/Form1.vb
@@ -0,0 +1,16 @@
+﻿Imports System
+Imports System.Windows.Forms
+
+Namespace PdfViewerSample
+    Partial Public Class Form1
+        Inherits Form
+
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+
+        Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) Handles MyBase.Load
+            Me.pdfViewer1.LoadDocument("..\..\Report.pdf")
+        End Sub
+    End Class
+End Namespace
