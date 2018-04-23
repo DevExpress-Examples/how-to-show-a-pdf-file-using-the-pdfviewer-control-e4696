c520715361296d4d4b149d33bdfb0f9e849c12f4
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/PdfViewerSample/Form1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace PdfViewerSample {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            this.pdfViewer1.LoadDocument(@"..\..\Report.pdf");
+
+        }
+    }
+}
